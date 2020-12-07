29e11b686173733bb64cdb4cca03cd7f9079e5e0
Niklas Blomqvist <niklas.blomqvist@edu.turkuamk.fi>
2020-12-07 15:17:12 +0200

    Dropdown maalin valinnalle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+}
+
 
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
   const handleSubmit = event => {
     event.preventDefault();
-    alert('Kaavake lähetetty.')
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 3000)
   }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    })
+  }
+
   return (
     <div className="wrapper">
       <h1>Nimesi</h1>
+      {submitting &&
+      <div>Lähetät seuraavat tiedot:
+        <ul>
+          {Object.entries(formData).map(([name, value]) => (
+            <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+          ))}
+        </ul>
+      </div>
+      }
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
             <p>Nimi</p>
-            <input name="name"/>
+            <input name="name" onChange={handleChange}/>
           </label>
         </fieldset>
-        <button type="submit">Lähetä"</button>
+        <fieldset>
+          <label>
+            <p>Maalit</p>
+            <select name="maali" onChange={handleChange}>
+              <option value="">--Valitse maali--</option>
+              <option value="Tapettimaali">Tapettimaali</option>
+              <option value="Kattomaali">Kattomaali</option>
+              <option value="Puumaali">Puumaali</option>
+              <option value="Öljymaali">Öljymaali</option>
+            </select>
+          </label>
+          <label>
+            <p>Litra</p>
+            <input type="number" name="count" onChange={handleChange} step="1"/>
+          </label>
+        </fieldset>
+        <button type="submit">Lähetä</button>
       </form>
     </div>
   );
