9c5b75b6a91960ce7032e291b852e783a9d2427b
Oskar Lehto <oskar.lehto@edu.turkuamk.fi>
2020-12-12 14:03:55 +0200

    komponentit erikseen/muutoksia formiin ja appiin

--- src/App.js
+++ src/App.js
@@ -1,81 +1,28 @@
-import React, { useReducer, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const formReducer = (state, event) => {
-  return {
-    ...state,
-    [event.name]: event.value
-  }
-}
+import React, { Component } from 'react';
+import Form from './components/Form';
+import Perustiedot from "./components/Perustiedot"
 
 
-function App() {
-  const [formData, setFormData] = useReducer(formReducer, {});
-  const [submitting, setSubmitting] = useState(false);
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    setSubmitting(true);
-
-    setTimeout(() => {
-      setSubmitting(false);
-    }, 3000)
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
   }
 
-  const handleChange = event => {
-    setFormData({
-      name: event.target.name,
-      value: event.target.value,
-      price: event.target.price,
-    })
-  }
-
-  return (
-    <div className="wrapper">
-      <h1>Maalin perustiedot</h1>
-      {submitting &&
-      <div>Lähetät seuraavat tiedot:
-        <ul>
-          {Object.entries(formData).map(([name, value]) => (
-            <li key={name}><strong>{name}</strong>:{value.toString()}</li>
-          ))}
-        </ul>
-      </div>
-      }
-      <form onSubmit={handleSubmit}>
+  render() {
+    return (
+      <div>
+        <Form name={this.state.name} />
+      
+       
         
-        <fieldset>
-          <label>
-            <p>Maalit</p>
-            <select name="maali" onChange={handleChange}>
-              <option value="">--Valitse maali--</option>
-              <option value="Tapettimaali">Tapettimaali</option>
-              <option value="Kattomaali">Kattomaali</option>
-              <option value="Puumaali">Puumaali</option>
-              <option value="Öljymaali">Öljymaali</option>
-            </select>
-          </label>
-          <label>
-            <p>Hinta (€/l)</p>
-            <input type="number" name="price" onChange={handleChange} step="1"/>
-            <p>Riittoisuus (m2/l)</p>
-            <input type="number" name="count" onChange={handleChange} step="1"/>
-          </label>
-          <label>
-            <p>Kuinka monta maalauskertaa?</p>
-            <select name="maalauskerta" onChange={handleChange}>
-              <option value="">--Kuinka monta krt--</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-          </label>
-        </fieldset>
-        <button type="submit">Lähetä</button>
-      </form>
-    </div>
-  );
-}
 
+      </div>
+    );
+  }
+}
 export default App;
+
